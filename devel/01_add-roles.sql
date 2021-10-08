cd84db48eca2dd03a76e21fea3ddc7487bee9983
Andrea Stella <andreaste00@gmail.com>
2021-10-08 21:18:14 +0200

    Adjust database minimum points for roles.

--- devel/01_add-roles.sql
+++ devel/01_add-roles.sql
@@ -1,5 +1,5 @@
-INSERT INTO role (id, name, min_points) VALUES ('885600227158028309', 'entryPan', 0);
-INSERT INTO role (id, name, min_points) VALUES ('894205386138193972', 'commonPan', 10);
-INSERT INTO role (id, name, min_points) VALUES ('894205731128090684', 'rarePan', 100);
-INSERT INTO role (id, name, min_points) VALUES ('894206649877815296', 'epicPan', 1000);
-INSERT INTO role (id, name, min_points) VALUES ('894206764311015424', 'legendaryPan', 10000);
+INSERT INTO role (id, name, min_points) VALUES ('885600227158028309', 'entryPan', 100);
+INSERT INTO role (id, name, min_points) VALUES ('894205386138193972', 'commonPan', 1000);
+INSERT INTO role (id, name, min_points) VALUES ('894205731128090684', 'rarePan', 10000);
+INSERT INTO role (id, name, min_points) VALUES ('894206649877815296', 'epicPan', 100000);
+INSERT INTO role (id, name, min_points) VALUES ('894206764311015424', 'legendaryPan', 1000000);
